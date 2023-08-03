99bad13280ef3f7d79c5254da86f68ebc36be0ec
MoonJung Jang <84329979+gamangee@users.noreply.github.com>
2023-08-03 21:14:14 +0900

    [initial] 프로젝트 생성 및 초기 세팅

---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    primary: "#007bff",
+    secondary: "#6c757d",
+    success: "#28a745",
+    error: "#dc3545",
+  },
+  fonts: {
+    main: "Arial, sans-serif",
+  },
+};
+
+export default theme;
